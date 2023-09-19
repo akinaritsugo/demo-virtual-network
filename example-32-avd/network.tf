003b5922b0eb52bd92146cbd45b961665c420597
Akinari Tsugo <akinaritsugo@microsoft.com>
2023-09-19 16:45:46 +0900

    RDP接続許可

--- example-32-avd/network.tf
+++ example-32-avd/network.tf
@@ -22,6 +22,18 @@ resource "azurerm_network_security_group" "adds" {
   name                = "${var.prj}-${var.env}-main-adds-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowAnyRDPInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Subnet configuration
