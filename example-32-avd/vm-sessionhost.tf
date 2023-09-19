18b03215f253b44ca5f4a5fcc6b8eb6bb782e8a3
Akinari Tsugo <akinaritsugo@microsoft.com>
2023-09-19 22:00:30 +0900

    手動作成のセッションホストは削除

--- example-32-avd/vm-sessionhost.tf
+++ example-32-avd/vm-sessionhost.tf
@@ -1,76 +1,76 @@
-resource "azurerm_windows_virtual_machine" "sessionhost" {
-  name                = "${var.prj}-${var.env}-sessionhost-vm"
-  computer_name       = "sessionhost-vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B2s"
-  admin_username      = var.username
-  admin_password      = var.password
+# resource "azurerm_windows_virtual_machine" "sessionhost" {
+#   name                = "${var.prj}-${var.env}-sessionhost-vm"
+#   computer_name       = "sessionhost-vm"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   size                = "Standard_B2s"
+#   admin_username      = var.username
+#   admin_password      = var.password
 
-  os_disk {
-    name                 = "${var.prj}-${var.env}-sessionhost-vm-osdisk"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#   os_disk {
+#     name                 = "${var.prj}-${var.env}-sessionhost-vm-osdisk"
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
 
-  source_image_reference {
-    publisher = "MicrosoftWindowsDesktop"
-    offer     = "windows-11"
-    sku       = "win11-22h2-avd"
-    version   = "latest"
-  }
+#   source_image_reference {
+#     publisher = "MicrosoftWindowsDesktop"
+#     offer     = "windows-11"
+#     sku       = "win11-22h2-avd"
+#     version   = "latest"
+#   }
 
-  network_interface_ids = [
-    azurerm_network_interface.sessionhost.id,
-  ]
+#   network_interface_ids = [
+#     azurerm_network_interface.sessionhost.id,
+#   ]
 
-  tags = {
-    project     = var.prj
-    environment = var.env
-  }
+#   tags = {
+#     project     = var.prj
+#     environment = var.env
+#   }
 
-  lifecycle {
-    ignore_changes = [
-      identity,
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       identity,
+#     ]
+#   }
+# }
 
-# NIC設定
-resource "azurerm_network_interface" "sessionhost" {
-  name                = "${var.prj}-${var.env}-sessionhost-vm-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# # NIC設定
+# resource "azurerm_network_interface" "sessionhost" {
+#   name                = "${var.prj}-${var.env}-sessionhost-vm-nic"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.desktop.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.sessionhost.id
-  }
-}
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.desktop.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.sessionhost.id
+#   }
+# }
 
-# Public IP
-resource "azurerm_public_ip" "sessionhost" {
-  name                = "${var.prj}-${var.env}-sessionhost-vm-ip"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  allocation_method   = "Dynamic"
+# # Public IP
+# resource "azurerm_public_ip" "sessionhost" {
+#   name                = "${var.prj}-${var.env}-sessionhost-vm-ip"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   allocation_method   = "Dynamic"
 
-  tags = {
-    project     = var.prj
-    environment = var.env
-  }
-}
+#   tags = {
+#     project     = var.prj
+#     environment = var.env
+#   }
+# }
 
-# 自動シャットダウン設定
-resource "azurerm_dev_test_global_vm_shutdown_schedule" "sessionhost" {
-  virtual_machine_id    = azurerm_windows_virtual_machine.sessionhost.id
-  location              = azurerm_resource_group.rg.location
-  enabled               = true
-  daily_recurrence_time = "0400"
-  timezone              = "Tokyo Standard Time"
-  notification_settings {
-    enabled = false
-  }
-}
+# # 自動シャットダウン設定
+# resource "azurerm_dev_test_global_vm_shutdown_schedule" "sessionhost" {
+#   virtual_machine_id    = azurerm_windows_virtual_machine.sessionhost.id
+#   location              = azurerm_resource_group.rg.location
+#   enabled               = true
+#   daily_recurrence_time = "0400"
+#   timezone              = "Tokyo Standard Time"
+#   notification_settings {
+#     enabled = false
+#   }
+# }
